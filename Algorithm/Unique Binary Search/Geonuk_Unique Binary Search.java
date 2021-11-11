3b91720fe6b9541dce4dbee2141de06e59297e71
Geon._.Uk <30423696+parkgunuk@users.noreply.github.com>
2021-11-11 23:53:01 +0900

    Update Geonuk_Unique Binary Search.java

--- Algorithm/Unique Binary Search/Geonuk_Unique Binary Search.java
+++ Algorithm/Unique Binary Search/Geonuk_Unique Binary Search.java
@@ -5,7 +5,7 @@ class Solution {
         
         for(int i = 2; i <= n ; ++i){
             for(int j = 0 ; j <] i - 1 ; ++j){
-                dp[i] = dp[j] + dp[i] * dp[i - j - 1];
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
         
