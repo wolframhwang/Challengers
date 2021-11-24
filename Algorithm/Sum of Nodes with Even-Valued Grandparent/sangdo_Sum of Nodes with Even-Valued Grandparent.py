e98591cd8db729dec735b05adbd75e7b9111fd8b
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-11-25 02:05:35 +0900

    Update sangdo_Sum of Nodes with Even-Valued Grandparent.py

--- Algorithm/Sum of Nodes with Even-Valued Grandparent/sangdo_Sum of Nodes with Even-Valued Grandparent.py
+++ Algorithm/Sum of Nodes with Even-Valued Grandparent/sangdo_Sum of Nodes with Even-Valued Grandparent.py
@@ -1,9 +1,6 @@
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode, parent = 1, grandParent = 1) -> int:
-        ret = 0
         if not root: 
             return 0
-        if grandParent % 2 == 0: 
-            ret += root.val
         
-        return ret + self.sumEvenGrandparent(root.left, root.val, parent) + self.sumEvenGrandparent(root.right, root.val, parent)
+        return self.sumEvenGrandparent(root.left, root.val, parent) + self.sumEvenGrandparent(root.right, root.val, parent) + (root.val if grandParent%2 == 0 else 0)
