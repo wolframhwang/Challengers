f5a5c16a54e72cbfc08f85b00b7508f8fc41a576
Jiwoong Hwang <hzw94@naver.com>
2021-12-16 08:55:34 +0900

    Create Wolfram_Insertion Sort List.swift

---
+++ Algorithm/Insertion Sort List/Wolfram_Insertion Sort List.swift
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func insertionSortList(_ head: ListNode?) -> ListNode? {
+        guard let head = head else { return nil }
+        let lead = ListNode(Int.min)
+        lead.next = head
+
+        var end = head
+        while let p = end.next {
+            if p.val < end.val {
+                var prev = lead
+				var cur = lead.next
+                while curr!.val <= p.val {
+                    prev = cur!
+                    cur = cur!.next
+                }
+                end.next = p.next
+                prev.next = p
+                p.next = cur
+            } else {
+                end = p
+            }
+        }
+        
+        return lead.next
+    }
+}
