b6c0e11e87300742e6e49b1b0d2564e47febdb0c
Jiwoong Hwang <hzw94@naver.com>
2021-12-28 12:52:50 +0900

    Update Wolfram_Jump Game.swift

--- Algorithm/Jump Game/Wolfram_Jump Game.swift
+++ Algorithm/Jump Game/Wolfram_Jump Game.swift
@@ -2,15 +2,15 @@
 // use DP by flow from up to down
 class Solution {
     func canJump(_ nums: [Int]) -> Bool {
-		var dist = 0
+	var dist = 0
         for i in (0..<nums.count).reversed() {
-			if nums[i] >= dist {
-				dist = 1
-			}else {
-				dist += 1
-			}			
-		}
-		return dist == 1
+		if nums[i] >= dist {
+			dist = 1
+		}else {
+			dist += 1
+		}			
+	}
+	return dist == 1
 			
     }
 }
